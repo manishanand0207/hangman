07dfd67275f8b964271101a8180392b93595842f
Manish <66215707+manishanand0207@users.noreply.github.com>
2025-05-04 13:42:58 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,122 @@
+import random
+
+word_list = ["Agra", "Ajmer", "Alwar", "Bhuj", "Delhi", "Daman", "Gaya", "Goa", "Imphal", "Kochi", "Kota", "Mandi", "Patna", "Pune", "Salem", "Shimla", "Surat", "Udupi", "Vapi"]
+
+
+stages = ['''
+      _______
+     |/      |
+     |      (_)
+     |      /|/
+     |       |
+     |      / /
+     |
+ ____|____
+''', '''
+      _______
+     |/      |
+     |      (_)
+     |      /|/
+     |       |
+     |      / 
+     |
+ ____|____
+''', '''
+      _______
+     |/      |
+     |      (_)
+     |      /|/
+     |       |
+     |      
+     |
+ ____|____
+''', '''
+      _______
+     |/      |
+     |      (_)
+     |      /|/
+     |       
+     |      
+     |
+ ____|____
+ ''', '''
+      _______
+     |/      |
+     |      (_)
+     |      
+     |       
+     |      
+     |
+ ____|____
+''', '''
+      _______
+     |/      |
+     |      
+     |      
+     |       
+     |      
+     |
+ ____|____
+''']
+
+print('''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/     
+''')
+
+print("Guess the names of Indian Cities and save the hangman!!")
+
+lives = len(stages) - 1
+choosen_word = random.choice(word_list)
+word_length = len(choosen_word)
+
+#print(f"Chosen word: {choosen_word}")
+
+
+#Guessing a letter
+placeholder =  ["_"] * word_length
+print("".join(placeholder))
+
+#for position in range(word_length):
+#    placeholder += "_"
+#print(placeholder)
+
+
+game_over = False
+correct_letter = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    print(guess)
+
+    found = False
+
+    #finding out the guess is correct or not
+    for letter in range(word_length):
+        if choosen_word[letter].lower() == guess:
+             placeholder[letter] = choosen_word[letter]
+             found = True
+    print("".join(placeholder))
+    print(f"lives left {lives}")
+
+
+
+    if guess not in choosen_word:
+        lives -= 1
+        if lives < 0:
+            game_over = True
+            print("*===========================You Lose===========================*")
+            print("The word was:", choosen_word)
+
+    stage_index = lives if lives >= 0 else 0
+    print(stages[stage_index])
+
+    if "_" not in placeholder:
+        game_over=True
+        print("*===========================You Win===========================*")
